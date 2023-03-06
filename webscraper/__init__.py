40172bc0c0118c7f8d86d687a02d99ba8438def8
jisphording <mail@johannesisphording.de>
2023-03-06 01:27:22 +0100

    [init][main] get html data to work with

---
+++ webscraper/__init__.py
@@ -0,0 +1,28 @@
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# SCRAPER
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+#
+# This is a custom scraping tool to pull content from websites that are transitioned
+# to another CMS
+#
+# (C) 2023 - Johannes Isphording
+
+# Import custom modules
+import url_to_extract
+import scraper
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# SETTINGS
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+# URL of the web page to extract
+url = url_to_extract.url()
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# DEFINING FUNCTIONS
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+# Get HTML
+html = scraper.get_html( url )
+
+scraper.write_html( html, './data/' )
