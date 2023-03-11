7ab67a542a42993d1849addded865788d4ace3ac
jisphording <mail@johannesisphording.de>
2023-03-11 23:03:14 +0100

    [init] created new module to transform scrape data

---
+++ webscraper/transform_data/__init__.py
@@ -0,0 +1,6 @@
+from .convert_date_to_webflow import convert_date_to_webflow
+from .extract_date_from_string import extract_date
+from .extract_dates_from_json import extract_dates_from_json
+from .load_json import load_json
+from .load_webflow_csv import load_webflow_csv
+from .import_csv import import_csv
