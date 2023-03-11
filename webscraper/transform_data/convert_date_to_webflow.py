9348eccc14a238a8b4d15e012fb7cd52965a266b
jisphording <mail@johannesisphording.de>
2023-03-12 00:04:48 +0100

    [module] updated main to iterate in place

--- webscraper/transform_data/convert_date_to_webflow.py
+++ webscraper/transform_data/convert_date_to_webflow.py
@@ -3,7 +3,10 @@ import pytz
 # CONVERT DATE TO WEBFLOW 
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 #
-def convert_date_to_webflow( date ):
+def convert_date_to_webflow( df, column='Datum' ):
+    # SETTINGS
+    webflow_date_df = df
+
     # METHODS
     def convert_date( date ):
         # convert the date to the timezone of the webflow CMS
@@ -15,6 +18,15 @@ def convert_date_to_webflow( date ):
 
         return local_date.astimezone( target_tz )
 
-    webflow_date_format = convert_date( date ).strftime( '%a %h %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)' )
+    # MAIN PROGRAM
+    # iterate over the dataframe and convert the dates in the specified column to the webflow timezone format
+    for i in range( len( df[ column ] ) ):
+        try:
+            # if a date was found, convert it to the webflow timezone
+            date = df[ column ][ i ]
+            df[ column ][ i ] = convert_date( date ).strftime( '%a %h %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)' )
+        except:
+            # if no date was found, insert an empty string
+            date = ''
 
-    return webflow_date_format
+    return webflow_date_df
