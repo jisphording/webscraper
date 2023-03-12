55865615b1b4bc2ed755bcd8f0c98ef58d7fa340
jisphording <mail@johannesisphording.de>
2023-03-12 01:53:17 +0100

    [init][main][workflow] added transform data workflow

---
+++ webscraper/transform_data.py
@@ -0,0 +1,37 @@
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# TRANSFORM DATA
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+#
+# This tool transforms the data from the web scraper into a format that can be ingested 
+# into the Webflow CMS.
+#
+# (C) 2023 - Johannes Isphording
+
+# Import custom modules
+import url_to_extract
+import transform_data
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# SETTINGS
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+# URL of the web page to extract
+path = url_to_extract.path()
+json = url_to_extract.json()
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# MAIN PROGRAM
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+df_webflow = transform_data.load_webflow_csv( path )
+df_json = transform_data.load_json( json )
+
+extracted_dates = transform_data.extract_dates_from_json( df_json )
+transform_data.convert_date_to_webflow( extracted_dates )
+
+df_combined = df_webflow.append( extracted_dates, ignore_index=True )
+
+print( df_combined )
+# [ print(x) for x in extracted_dates ]
+
+# save the combined dataframe as a csv file
+df_combined.to_csv( 'combined.csv', index=False )
